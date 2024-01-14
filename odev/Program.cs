a29becc7bd6c7a3a2e969bf85569279a674f4731
canumut05 <hasozdemirumutcan@gmail.com>
2024-01-14 14:28:30 +0300

    Proje dosyası ekle.

---
+++ odev/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace or259
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string s, u = "";
+            int a;
+            byte c;
+
+            Console.Write("Bir cümle giriniz: ");
+            s = Console.ReadLine();
+
+            for (a = 0; a < s.Length; a++)
+            {
+                c = (byte)s[a];
+
+                if (c >= 65 && c <= 90) 
+                {
+                    u = u + (char)(c + 32); 
+                }
+                else
+                {
+                    u = u + s[a];
+                }
+            }
+
+            Console.WriteLine(u);
+            
+           
+        }
+    }
+}
